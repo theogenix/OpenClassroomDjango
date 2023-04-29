1ba76c121355735f002f415b5544a58f28719aa0
theo <theogenix@gmail.com>
2023-04-29 18:25:29 +0200

    ajout modele Listing et Band

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -8,17 +8,24 @@ class Band(models.Model):
         HIP_HOP='HH'
         SYNTH_POP='SP'
         ALTERNATIVE_ROCK='AR'
-    genre=models.fields.CharField(choices=Genre.choices,max_length=5)
 
-
-name=models.fields.CharField(max_length=100)
-genre=models.fields.CharField(max_length=50)
-biography=models.fields.CharField(max_length=1000)
-year_formed=models.fields.IntegerField(
-    validators=[MinValueValidator(1900),MaxValueValidator(2021)]
-)
-active=models.fields.BooleanField(default=True)
-official_homepage=models.fields.URLField(null=True,blank=True)
+    name=models.fields.CharField(max_length=100)
+    genre=models.fields.CharField(choices=Genre.choices,max_length=5,default='HH')
+    biography=models.fields.CharField(max_length=1000,default='')
+    year_formed=models.fields.IntegerField(
+        validators=[MinValueValidator(1900),MaxValueValidator(2021)],default=2000
+    )
+    active=models.fields.BooleanField(default=True)
+    official_homepage=models.fields.URLField(null=True,blank=True)
 
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        disques='Records'
+        vetements='Clothing'
+        affiches='Posters'
+        divers='Miscellaneous'
     name=models.fields.CharField(max_length=100)
+    description=models.fields.CharField(max_length=1000)
+    sold=models.fields.BooleanField(default=True)
+    year=models.fields.IntegerField(default=2000)
+    type=models.fields.CharField(choices=Type.choices,max_length=20,default='Records')
