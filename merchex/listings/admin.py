893be54ab00525e88a8279b87b6e089e017c49bb
theo <theogenix@gmail.com>
2023-04-29 23:54:09 +0200

    CRUD dans admin de django

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+from listings.models import Listing
+
+class BandAdmin(admin.ModelAdmin):
+    list_display=('name','year_formed','genre')
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display=('name','type','year')
+
+
+admin.site.register(Band,BandAdmin)
+admin.site.register(Listing,ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -8,7 +8,9 @@ class Band(models.Model):
         HIP_HOP='HH'
         SYNTH_POP='SP'
         ALTERNATIVE_ROCK='AR'
-
+    def __str__(self):
+        return f'{self.name}'
+        
     name=models.fields.CharField(max_length=100)
     genre=models.fields.CharField(choices=Genre.choices,max_length=5,default='HH')
     biography=models.fields.CharField(max_length=1000,default='')
@@ -27,5 +29,8 @@ class Listing(models.Model):
     name=models.fields.CharField(max_length=100)
     description=models.fields.CharField(max_length=1000)
     sold=models.fields.BooleanField(default=True)
-    year=models.fields.IntegerField(default=2000)
+    year = models.IntegerField(
+        validators=[MinValueValidator(1900),MaxValueValidator(2021)],default=2000
+        )
     type=models.fields.CharField(choices=Type.choices,max_length=20,default='Records')
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
